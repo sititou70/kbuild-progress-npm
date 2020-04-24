7d6d189f565fd61577c79146e50599482b3c3158
Takuya Shizukuishi <sititou70@gmail.com>
2020-04-25 08:49:13 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# kbuild-progress
+
+The progress bar for Linux kernel building.
+
+## install
+
+`npm install -g kbuild-progress`
+
+## usage
+
+`kbuild-progress [kernel make arguments...]`
+
+1. Move to your Linux kernel source tree.
+1. Setup .config file and make kernel.
+1. Run kbuild-progress, then this tool will begin to monitor kernel building progress. I recommend doing this step in another terminal session(eg. another terminal tab, tmux window or pane).
+
+### example
+
+```
+[tab1]
+$ cd linux-5.6.7
+$ make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- defconfig
+$ make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j4 vmlinux
+```
+
+```
+[tab2]
+$ cd linux-5.6.7
+$ kbuild-progress ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j4 vmlinux
+```
+
+## Licence
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+import path from 'path';
+import { spawn, execSync } from 'child_process';
+import byline from 'byline';
+import Progress from 'progress';
+
+const sleep = (ms: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
+const getTargetFiles = async (): Promise<string[]> =>
+  new Promise((resolve) => {
+    let target_files: string[] = [];
+
+    const make = spawn('make', ['-n', ...process.argv.slice(2)]);
+    byline(make.stdout).on('data', (buff: Buffer) => {
+      const line = buff.toString();
+      const matched = line.match(/-o ([^ ,]+?\.o)/);
+
+      if (matched === null) return;
+      target_files.push(matched[1]);
+    });
+    make.on('exit', () => {
+      resolve(target_files);
+    });
+    make.on('error', (e) => {
+      console.error('make process exit with error!', e);
+      throw new Error("can't get target files.");
+    });
+  });
+
+const getChangedFiles = (): string[] =>
+  execSync('find . -newer .config').toString().split('\n');
+
+const main = async (): Promise<void> => {
+  try {
+    fs.statSync('.config');
+  } catch (e) {
+    console.error(
+      'this tool must be executed in the Linux kernel project root.',
+      e
+    );
+    throw Error("can't stat .config file");
+  }
+
+  const target_files = new Set(
+    (await getTargetFiles()).map((x) => path.normalize(x))
+  );
+  const progress = new Progress('[:bar] :percent (:current / :total) :etas', {
+    total: target_files.size,
+  });
+
+  while (true) {
+    try {
+      const chanaged_files: string[] = getChangedFiles().map((x) =>
+        path.normalize(x)
+      );
+      const remaining_files: string[] = chanaged_files.filter((x) =>
+        target_files.has(x)
+      );
+      progress.tick(remaining_files.length - progress.curr);
+    } catch (e) {}
+
+    await sleep(3000);
+  }
+};
+main();
